8d51904680f1c9dda1d2d39c3da434e7a129a627
Jlaborne <laborne.j@hotmail.fr>
2025-04-15 14:24:56 +0200

    T1

---
+++ TypeScript/task_1/js/main.ts
@@ -0,0 +1,19 @@
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [attribute: string]: any;
+}
+
+//Test part:
+const teacher3: Teacher = {
+  firstName: "John",
+  fullTimeEmployee: false,
+  lastName: "Doe",
+  location: "London",
+  contract: false,
+};
+
+console.log(teacher3);
