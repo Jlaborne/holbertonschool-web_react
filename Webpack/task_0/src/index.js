6f6ac84c0fd166bf0ad6d41532c6185f3e466a9d
Joshua Laborne <laborne.j@hotmail.fr>
2025-04-17 11:59:32 +0200

    t0

--- Webpack/task_0/src/index.js
+++ Webpack/task_0/src/index.js
@@ -1,6 +1,6 @@
 import $ from "jquery";
 
-(function () {
+$(function () {
   $("body").append("<p>Holberton Dashboard</p>");
   $("body").append("<p>Dashboard data for the students</p>");
   $("body").append("<p>Copyright - Holberton School</p>");
