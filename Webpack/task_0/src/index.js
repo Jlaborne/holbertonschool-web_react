a5a810f092c7415fe194479d1461627bcf237324
Joshua Laborne <laborne.j@hotmail.fr>
2025-04-17 11:39:45 +0200

    Task0

---
+++ Webpack/task_0/src/index.js
@@ -0,0 +1,7 @@
+import $ from "jquery";
+
+$(document).ready(function () {
+  $("body").append("<p>Paragraph 1 - Hello from Webpack!</p>");
+  $("body").append("<p>Paragraph 2 - Webpack is cool!</p>");
+  $("body").append("<p>Paragraph 3 - jQuery works!</p>");
+});
