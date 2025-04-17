c96e72881ad1eb024d41f18515dce3af78b64512
Joshua Laborne <laborne.j@hotmail.fr>
2025-04-17 11:49:13 +0200

    t0

--- Webpack/task_0/src/index.js
+++ Webpack/task_0/src/index.js
@@ -1,5 +1,7 @@
 import $ from "jquery";
 
-$("body").append("<p>Holberton Dashboard</p>");
-$("body").append("<p>Dashboard data for the students</p>");
-$("body").append("<p>Copyright - Holberton School</p>");
+$(function () {
+  $("body").append("<p>Paragraph 1 - Hello from Webpack!</p>");
+  $("body").append("<p>Paragraph 2 - Webpack is cool!</p>");
+  $("body").append("<p>Paragraph 3 - jQuery works!</p>");
+});
