178e34228c5cb812b49a4450aae288fa9b90cbf4
Joshua Laborne <laborne.j@hotmail.fr>
2025-04-23 13:22:04 +0200

    task4 test for the form

--- react_intro/task_2/dashboard/src/App.spec.js
+++ react_intro/task_2/dashboard/src/App.spec.js
@@ -31,4 +31,20 @@ describe("App component", () => {
   test("renders the logo image", () => {
     expect(screen.getByAltText(/holberton logo/i)).toBeInTheDocument();
   });
+
+  test("renders 2 input elements", () => {
+    const inputs = screen.getAllByRole("textbox");
+    const password = screen.getByLabelText(/password/i);
+    expect(inputs.length).toBe(1);
+    expect(password).toBeInTheDocument();
+  });
+
+  test("renders 2 label elements with correct text", () => {
+    expect(screen.getByLabelText(/email/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/password/i)).toBeInTheDocument();
+  });
+
+  test("renders a button with text OK", () => {
+    expect(screen.getByRole("button", { name: /ok/i })).toBeInTheDocument();
+  });
 });
