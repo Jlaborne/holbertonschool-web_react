c7f70013d459ec2c7ab36531e1dc7a350528932b
Jlaborne <laborne.j@hotmail.fr>
2025-04-29 11:27:24 +0200

    task0

---
+++ react_props/task_0/dashboard/src/Notifications/Notifications.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import "./Notifications.css";
+import closeIcon from "../assets/close-icon.png";
+import { getLatestNotification } from "../utils/utils";
+
+function Notifications() {
+  const handleClick = () => console.log("Close button has been clicked");
+
+  return (
+    <div className="notifications">
+      <button
+        style={{
+          position: "absolute",
+          top: "10px",
+          right: "10px",
+          background: "none",
+          border: "none",
+          cursor: "pointer",
+        }}
+        aria-label="Close"
+        onClick={handleClick}
+      >
+        <img
+          src={closeIcon}
+          alt="close icon"
+          style={{ height: "15px", width: "15px" }}
+        />
+      </button>
+
+      <p>Here is the list of notifications</p>
+      <ul>
+        <li data-priority="default">New course available</li>
+        <li data-priority="urgent">New resume available</li>
+        <li
+          data-priority="urgent"
+          dangerouslySetInnerHTML={{ __html: getLatestNotification() }}
+        />
+      </ul>
+    </div>
+  );
+}
+
+export default Notifications;
