a3a557a5d01f1bda7e74c18d51c4aa70ed6484ed
Joshua Laborne <laborne.j@hotmail.fr>
2025-04-30 15:15:37 +0200

    test

--- react_intro/task_2/dashboard/src/App.spec.js
+++ react_intro/task_2/dashboard/src/App.spec.js
@@ -32,12 +32,16 @@ describe("App component", () => {
     expect(screen.getByAltText(/holberton logo/i)).toBeInTheDocument();
   });
 
-  test("renders 2 input elements", () => {
-    const inputs = screen.getAllByLabelText(/./);
-    expect(inputs.length).toBe(2);
+  test("renders two input elements", () => {
+    render(<App />);
+    const emailInput = screen.getByLabelText(/email/i);
+    const passwordInput = screen.getByLabelText(/password/i);
+    expect(emailInput).toBeInTheDocument();
+    expect(passwordInput).toBeInTheDocument();
   });
 
   test("renders two label elements with text Email and Password", () => {
+    render(<App />);
     const emailLabel = screen.getByLabelText(/email/i);
     const passwordLabel = screen.getByLabelText(/password/i);
     expect(emailLabel).toBeInTheDocument();
