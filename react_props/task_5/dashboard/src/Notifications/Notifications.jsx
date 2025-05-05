21eaa36f37332ebcef9c3b625e1b3a94b2533b78
Joshua Laborne <laborne.j@hotmail.fr>
2025-05-05 11:25:50 +0200

    t5

---
+++ react_props/task_5/dashboard/src/Notifications/Notifications.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./Notifications.css";
+import closeIcon from "../assets/close-icon.png";
+import NotificationItem from "./NotificationItem";
+
+function Notifications({ displayDrawer = false, notifications = [] }) {
+  const handleClick = () => console.log("Close button has been clicked");
+
+  return (
+    <>
+      <div className="notifications-title">
+        <p>Your notifications</p>
+      </div>
+
+      {displayDrawer && (
+        <div className="notifications">
+          <button
+            style={{
+              position: "absolute",
+              top: "10px",
+              right: "10px",
+              background: "none",
+              border: "none",
+              cursor: "pointer",
+            }}
+            aria-label="Close"
+            onClick={handleClick}
+          >
+            <img
+              src={closeIcon}
+              alt="close icon"
+              style={{ height: "15px", width: "15px" }}
+            />
+          </button>
+
+          {notifications.length === 0 ? (
+            <p>No new notification for now</p>
+          ) : (
+            <>
+              <p>Here is the list of notifications</p>
+              <ul>
+                {notifications.map((notif) => (
+                  <NotificationItem
+                    key={notif.id}
+                    type={notif.type}
+                    value={notif.value}
+                    html={notif.html}
+                  />
+                ))}
+              </ul>
+            </>
+          )}
+        </div>
+      )}
+    </>
+  );
+}
+
+Notifications.propTypes = {
+  displayDrawer: PropTypes.bool,
+  notifications: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      type: PropTypes.string,
+      value: PropTypes.string,
+      html: PropTypes.shape({ __html: PropTypes.string }),
+    })
+  ),
+};
+
+export default Notifications;
