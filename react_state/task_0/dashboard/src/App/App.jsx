634ebbf69c7fa781b23166d0d13f20d569a17d75
Jlaborne <laborne.j@hotmail.fr>
2025-05-21 13:54:57 +0200

    test

--- react_state/task_0/dashboard/src/App/App.jsx
+++ react_state/task_0/dashboard/src/App/App.jsx
@@ -1,16 +1,16 @@
-import React from "react";
+import React, { Component } from "react";
 import Notifications from "../Notifications/Notifications";
 import Header from "../Header/Header";
-import Footer from "../Footer/Footer";
 import Login from "../Login/Login";
+import Footer from "../Footer/Footer";
+import { getLatestNotification } from "../utils/utils";
+import CourseList from "../CourseList/CourseList";
 import BodySection from "../BodySection/BodySection";
 import BodySectionWithMarginBottom from "../BodySection/BodySectionWithMarginBottom";
-import CourseList from "../CourseList/CourseList";
-import PropTypes from "prop-types";
-import { getLatestNotification } from "../utils/utils";
+import WithLogging from "../HOC/WithLogging";
 import { StyleSheet, css } from "aphrodite";
 
-class App extends React.Component {
+class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -20,11 +20,7 @@ class App extends React.Component {
 
   static defaultProps = {
     logOut: () => {},
-  };
-
-  static propTypes = {
-    logOut: PropTypes.func,
-    isLoggedIn: PropTypes.bool,
+    isLoggedIn: false,
   };
 
   handleDisplayDrawer = () => {
@@ -35,8 +31,8 @@ class App extends React.Component {
     this.setState({ displayDrawer: false });
   };
 
-  handleKeyDown = (event) => {
-    if (event.ctrlKey && event.key === "h") {
+  handleKeyDown = (e) => {
+    if (e.ctrlKey && e.key === "h") {
       alert("Logging you out");
       this.props.logOut();
     }
@@ -51,68 +47,69 @@ class App extends React.Component {
   }
 
   render() {
+    const { isLoggedIn } = this.props;
+    const { displayDrawer } = this.state;
+
+    const coursesList = [
+      { id: 1, name: "ES6", credit: 60 },
+      { id: 2, name: "Webpack", credit: 20 },
+      { id: 3, name: "React", credit: 40 },
+    ];
+
     const notificationsList = [
       { id: 1, type: "default", value: "New course available" },
       { id: 2, type: "urgent", value: "New resume available" },
       { id: 3, type: "urgent", html: { __html: getLatestNotification() } },
     ];
-    const coursesList = [
-      { id: 1, name: "ES6", credit: "60" },
-      { id: 2, name: "Webpack", credit: "20" },
-      { id: 3, name: "React", credit: "40" },
-    ];
-    const { isLoggedIn } = this.props;
+
+    const LoginWithLogging = WithLogging(Login);
+    const CourseListWithLogging = WithLogging(CourseList);
 
     return (
-      <React.Fragment>
-        <div className={css(styles.app)}>
-          <div className={css(styles.notifications)}>
-            <Notifications
-              notifications={notificationsList}
-              displayDrawer={this.state.displayDrawer}
-              handleDisplayDrawer={this.handleDisplayDrawer}
-              handleHideDrawer={this.handleHideDrawer}
-            />
-          </div>
-          <Header />
-          <div className={css(styles.body)}>
-            {isLoggedIn ? (
-              <BodySectionWithMarginBottom title="Course list">
-                <CourseList courses={coursesList} />
-              </BodySectionWithMarginBottom>
-            ) : (
-              <BodySectionWithMarginBottom title="Log in to continue">
-                <Login />
-              </BodySectionWithMarginBottom>
-            )}
-            <BodySection title="News from the School">
-              <p>Holberton School News goes here</p>
-            </BodySection>
-          </div>
-          <Footer />
+      <>
+        <div className="root-notifications">
+          <Notifications
+            notifications={notificationsList}
+            displayDrawer={displayDrawer}
+            handleDisplayDrawer={this.handleDisplayDrawer}
+            handleHideDrawer={this.handleHideDrawer}
+          />
         </div>
-      </React.Fragment>
+        <Header />
+        <div className={css(styles.body)}>
+          {isLoggedIn ? (
+            <BodySectionWithMarginBottom title="Course list">
+              <CourseListWithLogging courses={coursesList} />
+            </BodySectionWithMarginBottom>
+          ) : (
+            <BodySectionWithMarginBottom title="Log in to continue">
+              <LoginWithLogging />
+            </BodySectionWithMarginBottom>
+          )}
+          <BodySection title="News from the School">
+            <p>Holberton School News goes here</p>
+          </BodySection>
+        </div>
+        <footer className={css(styles.footer)}>
+          <p>Copyright 2025 - Holberton School</p>
+        </footer>
+      </>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  app: {
-    margin: "0",
-    minHeight: "100vh",
-    display: "flex",
-    flexDirection: "column",
-  },
   body: {
-    flex: "1",
+    padding: "40px",
+    minHeight: "300px",
   },
-  notifications: {
-    display: "flex",
-    position: "absolute",
-    flexDirection: "column",
-    right: "0",
-    paddingRight: "1rem",
-    minWidth: "30rem",
+  footer: {
+    position: "fixed",
+    bottom: 0,
+    width: "100%",
+    borderTop: "3px solid #e1003c",
+    textAlign: "center",
+    padding: "1rem 0",
   },
 });
 
