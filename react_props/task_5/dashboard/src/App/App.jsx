589a18954cc6ee24d781b223ba5ed96cc63a66c5
Joshua Laborne <laborne.j@hotmail.fr>
2025-05-26 15:10:55 +0200

    test

--- react_props/task_5/dashboard/src/App/App.jsx
+++ react_props/task_5/dashboard/src/App/App.jsx
@@ -16,17 +16,17 @@ function App({ isLoggedIn = false }) {
 
   const notificationsList = [
     {
-      id: Math.floor(Math.random() * Date.now()),
+      id: 1,
       type: "default",
       value: "New course available",
     },
     {
-      id: Math.floor(Math.random() * Date.now()),
+      id: 2,
       type: "urgent",
       value: "New resume available",
     },
     {
-      id: Math.floor(Math.random() * Date.now()),
+      id: 3,
       type: "urgent",
       html: { __html: getLatestNotification() },
     },
