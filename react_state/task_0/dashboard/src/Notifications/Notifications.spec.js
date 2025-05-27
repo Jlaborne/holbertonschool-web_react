c61f0a7e7a85eee9354de00d2426acef8b6001e2
Joshua Laborne <laborne.j@hotmail.fr>
2025-05-27 23:51:55 +0200

    t0

--- react_state/task_0/dashboard/src/Notifications/Notifications.spec.js
+++ react_state/task_0/dashboard/src/Notifications/Notifications.spec.js
@@ -169,15 +169,4 @@ describe("Notifications component", () => {
 
     expect(handleHideDrawer).toHaveBeenCalled();
   });
-  const mockNotifications = [
-    { id: 1, type: "default", value: "New course available" },
-    { id: 2, type: "urgent", value: "New resume available" },
-    {
-      id: 3,
-      type: "urgent",
-      html: {
-        __html: "<strong>Urgent requirement</strong> - complete by EOD",
-      },
-    },
-  ];
 });
