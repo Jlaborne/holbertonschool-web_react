437a1d4f5f52a97090b7d6993c32916c2bc0bc8f
Joshua Laborne <laborne.j@hotmail.fr>
2025-05-28 11:53:31 +0200

    t0

--- react_state/task_0/dashboard/src/Notifications/Notifications.jsx
+++ react_state/task_0/dashboard/src/Notifications/Notifications.jsx
@@ -29,7 +29,11 @@ class Notifications extends React.Component {
     return (
       <>
         <div data-testid="menu-item" onClick={handleDisplayDrawer}>
-          Your notifications
+          {!displayDrawer && (
+            <div className={css(styles.menuItem)} onClick={handleDisplayDrawer}>
+              Your notifications
+            </div>
+          )}
         </div>
 
         {displayDrawer && (
@@ -46,16 +50,19 @@ class Notifications extends React.Component {
                 >
                   Close
                 </button>
-                <ul className={css(styles.ul)}>
-                  {notifications.map((notif) => (
-                    <NotificationItem
-                      key={notif.id}
-                      id={notif.id}
-                      type={notif.type}
-                      value={notif.value}
-                      html={notif.html}
-                      markAsRead={this.markAsRead}
-                    />
+                <ul style={{ listStyle: "none", padding: 0 }}>
+                  {notifications.map((notification) => (
+                    <li
+                      key={notification.id}
+                      onClick={() => this.markAsRead(notification.id)}
+                      style={{ cursor: "pointer" }}
+                    >
+                      {notification.value ? (
+                        notification.value
+                      ) : (
+                        <span dangerouslySetInnerHTML={notification.html} />
+                      )}
+                    </li>
                   ))}
                 </ul>
               </>
