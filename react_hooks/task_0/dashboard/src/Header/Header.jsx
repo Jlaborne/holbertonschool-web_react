78fd9361dd5d860fc755da405284e5328ea25631
Joshua Laborne <laborne.j@hotmail.fr>
2025-05-31 01:06:00 +0200

    Footer

--- react_hooks/task_0/dashboard/src/Header/Header.jsx
+++ react_hooks/task_0/dashboard/src/Header/Header.jsx
@@ -1,63 +1,38 @@
 import { useContext } from "react";
-import logo from "../assets/holberton-logo.jpg";
-//import { StyleSheet, css } from "aphrodite";
 import { newContext } from "../Context/context";
+// import { StyleSheet, css } from "aphrodite";
 
-function Header() {
-  const { user, logOut } = useContext(newContext);
+function Footer() {
+  const { user } = useContext(newContext);
 
   return (
     <div
-    //className={css(styles.header)}
+    // className={css(styles.footer)}
     >
-      <img
-        src={logo}
-        alt="holberton logo"
-        //className={css(styles.logo)}
-      />
-      <h1
-      //className={css(styles.title)}
-      >
-        School dashboard
-      </h1>
-      {user?.isLoggedIn && (
-        <section
-          id="logoutSection"
-          //className={css(styles.logout)}
-        >
-          Welcome {user.email}{" "}
-          <a href="#" onClick={logOut}>
-            logout
-          </a>
-        </section>
-      )}
+      <p>
+        Copyright 2025 - Holberton School
+        {user?.isLoggedIn && (
+          <span>
+            {" "}
+            - <a href="#">Contact us</a>
+          </span>
+        )}
+      </p>
     </div>
   );
 }
 
-/*const styles = StyleSheet.create({
-    header: {
-      backgroundColor: "white",
-      display: "flex",
-      alignItems: "center",
-      padding: "1rem",
-      borderBottom: "3px solid #e1003c",
-      position: "relative",
-    },
-    logo: {
-      height: "200px",
-    },
-    title: {
-      color: "#e1003c",
-      marginLeft: "1rem",
-    },
-    logout: {
-      position: "absolute",
-      right: "1rem",
-      top: "50%",
-      transform: "translateY(-50%)",
-      fontSize: "1rem",
-    },
-  });*/
+/*
+const styles = StyleSheet.create({
+  footer: {
+    position: "fixed",
+    bottom: 0,
+    width: "100%",
+    borderTop: "3px solid #e1003c",
+    textAlign: "center",
+    padding: "1rem 0",
+  },
+});
+*/
 
-export default Header;
+export default Footer;
