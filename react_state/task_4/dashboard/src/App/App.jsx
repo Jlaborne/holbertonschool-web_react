819b10f5a66842cf7d9d6a381c42f70c91ea30ea
Joshua Laborne <laborne.j@hotmail.fr>
2025-05-31 14:29:00 +0200

    footer update

--- react_state/task_4/dashboard/src/App/App.jsx
+++ react_state/task_4/dashboard/src/App/App.jsx
@@ -1,15 +1,15 @@
-import React, { Component } from "react";
-import Notifications from "../Notifications/Notifications";
-import Header from "../Header/Header";
-import Login from "../Login/Login";
-import Footer from "../Footer/Footer";
-import { getLatestNotification } from "../utils/utils";
-import CourseList from "../CourseList/CourseList";
-import BodySection from "../BodySection/BodySection";
-import BodySectionWithMarginBottom from "../BodySection/BodySectionWithMarginBottom";
-import WithLogging from "../HOC/WithLogging";
+import React, { Component } from 'react';
+import Notifications from '../Notifications/Notifications';
+import Header from '../Header/Header';
+import Login from '../Login/Login';
+import Footer from '../Footer/Footer';
+import { getLatestNotification } from '../utils/utils';
+import CourseList from '../CourseList/CourseList';
+import BodySection from '../BodySection/BodySection';
+import BodySectionWithMarginBottom from '../BodySection/BodySectionWithMarginBottom';
+import WithLogging from '../HOC/WithLogging';
 //import { StyleSheet, css } from "aphrodite";
-import { newContext as NewContext } from "../Context/context";
+import { newContext as NewContext } from '../Context/context';
 
 const LoginWithLogging = WithLogging(Login);
 const CourseListWithLogging = WithLogging(CourseList);
@@ -20,19 +20,19 @@ class App extends Component {
     this.state = {
       displayDrawer: false,
       user: {
-        email: "",
-        password: "",
+        email: '',
+        password: '',
         isLoggedIn: false,
       },
       notifications: [
-        { id: 1, type: "default", value: "New course available" },
-        { id: 2, type: "urgent", value: "New resume available" },
-        { id: 3, type: "urgent", html: { __html: getLatestNotification() } },
+        { id: 1, type: 'default', value: 'New course available' },
+        { id: 2, type: 'urgent', value: 'New resume available' },
+        { id: 3, type: 'urgent', html: { __html: getLatestNotification() } },
       ],
       courses: [
-        { id: 1, name: "ES6", credit: 60 },
-        { id: 2, name: "Webpack", credit: 20 },
-        { id: 3, name: "React", credit: 40 },
+        { id: 1, name: 'ES6', credit: 60 },
+        { id: 2, name: 'Webpack', credit: 20 },
+        { id: 3, name: 'React', credit: 40 },
       ],
     };
   }
@@ -41,8 +41,8 @@ class App extends Component {
   handleHideDrawer = () => this.setState({ displayDrawer: false });
 
   handleKeyDown = (e) => {
-    if (e.ctrlKey && e.key === "h") {
-      alert("Logging you out");
+    if (e.ctrlKey && e.key === 'h') {
+      alert('Logging you out');
       this.logOut();
     }
   };
@@ -60,8 +60,8 @@ class App extends Component {
   logOut = () => {
     this.setState({
       user: {
-        email: "",
-        password: "",
+        email: '',
+        password: '',
         isLoggedIn: false,
       },
     });
@@ -77,11 +77,11 @@ class App extends Component {
   };
 
   componentDidMount() {
-    document.addEventListener("keydown", this.handleKeyDown);
+    document.addEventListener('keydown', this.handleKeyDown);
   }
 
   componentWillUnmount() {
-    document.removeEventListener("keydown", this.handleKeyDown);
+    document.removeEventListener('keydown', this.handleKeyDown);
   }
 
   render() {
@@ -128,11 +128,7 @@ class App extends Component {
               <p>Holberton School News goes here</p>
             </BodySection>
           </div>
-          <Footer
-          //className={css(styles.footer)}
-          >
-            <p>Copyright 2025 - Holberton School</p>
-          </Footer>
+          <Footer />
         </>
       </NewContext.Provider>
     );
--- react_state/task_4/dashboard/src/Footer/Footer.jsx
+++ react_state/task_4/dashboard/src/Footer/Footer.jsx
@@ -1,12 +1,13 @@
-import React, { useContext } from "react";
-import { getCurrentYear, getFooterCopy } from "../utils/utils";
-import { newContext } from "../Context/context";
+import { useContext } from 'react';
+import { getCurrentYear, getFooterCopy } from '../utils/utils';
+import { newContext } from '../Context/context';
+import { StyleSheet, css } from 'aphrodite';
 
 function Footer() {
   const { user } = useContext(newContext);
 
   return (
-    <div className="App-footer">
+    <div className={css(styles.footer)}>
       <p>
         Copyright {getCurrentYear()} - {getFooterCopy(true)}
       </p>
@@ -18,5 +19,16 @@ function Footer() {
     </div>
   );
 }
+
+const styles = StyleSheet.create({
+  footer: {
+    position: 'fixed',
+    bottom: 0,
+    width: '100%',
+    borderTop: '3px solid #e1003c',
+    textAlign: 'center',
+    padding: '1rem 0',
+  },
+});
 
 export default Footer;
