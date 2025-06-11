36c0c24a40b813ac1c7e97607aad988608f6c3f9
Jlaborne <laborne.j@hotmail.fr>
2025-06-11 13:30:37 +0200

    Task6

--- react_redux-part1/task_3/dashboard/src/features/auth/authSlice.js
+++ react_redux-part1/task_3/dashboard/src/features/auth/authSlice.js
@@ -4,8 +4,8 @@ const initialState = {
   user: {
     email: "",
     password: "",
-    isLoggedIn: false,
   },
+  isLoggedIn: false,
 };
 
 const authSlice = createSlice({
@@ -16,12 +16,12 @@ const authSlice = createSlice({
       const { email, password } = action.payload;
       state.user.email = email;
       state.user.password = password;
-      state.user.isLoggedIn = true;
+      state.isLoggedIn = true;
     },
     logout: (state) => {
       state.user.email = "";
       state.user.password = "";
-      state.user.isLoggedIn = false;
+      state.isLoggedIn = false;
     },
   },
 });
