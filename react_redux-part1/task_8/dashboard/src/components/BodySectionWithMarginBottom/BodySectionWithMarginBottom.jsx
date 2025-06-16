b8d0dbb871bf297c467dd4d58d5f78b3d9bb0db5
Joshua Laborne <laborne.j@hotmail.fr>
2025-06-16 13:58:21 +0200

    task11

--- react_redux-part1/task_8/dashboard/src/components/BodySectionWithMarginBottom/BodySectionWithMarginBottom.jsx
+++ react_redux-part1/task_8/dashboard/src/components/BodySectionWithMarginBottom/BodySectionWithMarginBottom.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import { StyleSheet, css } from 'aphrodite';
 import BodySection from '../BodySection/BodySection';
 
 export default function BodySectionWithMarginBottom({ title, children }) {
   return (
     <div
-      style={{ marginBottom: '40px' }}
+      className={css(styles.marginBottom)}
       data-testid="body-section-with-margin"
     >
       <BodySection title={title}>{children}</BodySection>
     </div>
   );
 }
+
+const styles = StyleSheet.create({
+  marginBottom: {
+    marginBottom: 40,
+  },
+});
