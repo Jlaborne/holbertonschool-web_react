d070388d60f4115c745a151d9c2993e46083ed98
Joshua Laborne <laborne.j@hotmail.fr>
2025-06-16 12:39:09 +0200

    task10

--- react_redux-part1/task_7/dashboard/src/components/NotificationItem/NotificationItem.jsx
+++ react_redux-part1/task_7/dashboard/src/components/NotificationItem/NotificationItem.jsx
@@ -1,43 +1,51 @@
-import { memo } from "react";
+import { memo } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { markNotificationAsRead } from '../../features/notifications/notificationsSlice';
 
-const NotificationItem = memo(function NotificationItem({
-  type,
-  html,
-  value,
-  markAsRead,
-  id,
-}) {
+const NotificationItem = memo(function NotificationItem({ id }) {
+  const dispatch = useDispatch();
+  const notification = useSelector((state) =>
+    state.notifications.notifications.find((notif) => notif.id === id)
+  );
+  if (!notification) return null;
+
+  const { type, value, html } = notification;
+
+  const handleClick = () => {
+    dispatch(markNotificationAsRead(id));
+  };
+
   console.log(
     `Rendering NotificationItem with id: ${id}, type: ${type}, value: ${value}`
   );
-  if (type === "default") {
+  if (type === 'default') {
     return (
       <li
-        style={{ color: "blue" }}
+        style={{ color: 'blue' }}
         data-notification-type={type}
-        onClick={() => markAsRead(id)}
+        onClick={handleClick}
       >
         {value}
       </li>
     );
   }
 
-  if (type === "urgent" && html !== undefined) {
+  if (type === 'urgent' && html !== undefined) {
     return (
       <li
-        style={{ color: "red" }}
+        style={{ color: 'red' }}
         data-notification-type={type}
-        onClick={() => markAsRead(id)}
         dangerouslySetInnerHTML={html}
-      ></li>
+        onClick={handleClick}
+      />
     );
   }
 
   return (
     <li
-      style={{ color: "red" }}
+      style={{ color: 'red' }}
       data-notification-type={type}
-      onClick={() => markAsRead(id)}
+      onClick={handleClick}
     >
       {value}
     </li>
