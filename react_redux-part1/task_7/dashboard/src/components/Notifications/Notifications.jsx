ffe2569e31ad74db6e16f91b0c703a17e7ad5c3c
Joshua Laborne <laborne.j@hotmail.fr>
2025-06-16 11:50:23 +0200

    task 10

--- react_redux-part1/task_7/dashboard/src/components/Notifications/Notifications.jsx
+++ react_redux-part1/task_7/dashboard/src/components/Notifications/Notifications.jsx
@@ -1,59 +1,59 @@
-import { memo, useEffect } from "react";
-import { StyleSheet, css } from "aphrodite";
-import closeIcon from "../../assets/close-icon.png";
-import NotificationItem from "../NotificationItem/NotificationItem";
-import { useSelector, useDispatch } from "react-redux";
+import { memo, useEffect } from 'react';
+import { StyleSheet, css } from 'aphrodite';
+import closeIcon from '../../assets/close-icon.png';
+import NotificationItem from '../NotificationItem/NotificationItem';
+import { useSelector, useDispatch } from 'react-redux';
 import {
+  fetchNotifications,
   markNotificationAsRead,
   showDrawer,
   hideDrawer,
-  fetchNotifications,
-} from "../../features/notifications/notificationsSlice";
+} from '../../features/notifications/notificationsSlice';
 
 const styles = StyleSheet.create({
   notificationTitle: {
-    float: "right",
-    position: "absolute",
-    right: "10px",
-    top: "2px",
-    cursor: "pointer",
+    float: 'right',
+    position: 'absolute',
+    right: '10px',
+    top: '2px',
+    cursor: 'pointer',
   },
   notifications: {
-    border: "dotted",
-    borderColor: "crimson",
-    marginTop: "1%",
-    paddingLeft: "1rem",
-    marginBottom: "1rem",
-    width: "40%",
-    marginLeft: "59%",
+    border: 'dotted',
+    borderColor: 'crimson',
+    marginTop: '1%',
+    paddingLeft: '1rem',
+    marginBottom: '1rem',
+    width: '40%',
+    marginLeft: '59%',
   },
   notificationsButton: {
-    position: "absolute",
-    cursor: "pointer",
-    right: "5px",
-    top: "20px",
-    background: "transparent",
-    border: "none",
+    position: 'absolute',
+    cursor: 'pointer',
+    right: '5px',
+    top: '20px',
+    background: 'transparent',
+    border: 'none',
   },
   notificationTypeDefault: {
-    color: "blue",
+    color: 'blue',
   },
   notificationTypeUrgent: {
-    color: "red",
+    color: 'red',
   },
   menuItem: {
-    textAlign: "right",
+    textAlign: 'right',
   },
 });
 
 const Notifications = memo(function Notifications() {
   const dispatch = useDispatch();
 
-  const displayDrawer = useSelector(
-    (state) => state.notifications.displayDrawer
-  );
   const notifications = useSelector(
     (state) => state.notifications.notifications
+  );
+  const displayDrawer = useSelector(
+    (state) => state.notifications.displayDrawer
   );
 
   useEffect(() => {
@@ -67,22 +67,20 @@ const Notifications = memo(function Notifications() {
   return (
     <>
       <div
-        //className={css(styles.notificationTitle)}
+        className={css(styles.notificationTitle)}
         onClick={handleDisplayDrawer}
       >
         Your notifications
       </div>
       {displayDrawer && (
-        <div
-        //className={css(styles.notifications)}
-        >
+        <div className={css(styles.notifications)}>
           {notifications.length > 0 ? (
             <>
               <p>Here is the list of notifications</p>
               <button
                 onClick={handleHideDrawer}
                 aria-label="Close"
-                //className={css(styles.notificationsButton)}
+                className={css(styles.notificationsButton)}
               >
                 <img src={closeIcon} alt="close icon" />
               </button>
@@ -95,11 +93,11 @@ const Notifications = memo(function Notifications() {
                     value={notification.value}
                     html={notification.html}
                     markAsRead={() => handleMarkAsRead(notification.id)}
-                    /*className={
-                      notification.type === "urgent"
+                    className={
+                      notification.type === 'urgent'
                         ? css(styles.notificationTypeUrgent)
                         : css(styles.notificationTypeDefault)
-                    }*/
+                    }
                   />
                 ))}
               </ul>
