06bde0a5becd97d1ba11b12e9fd48ca887eb577f
Jlaborne <laborne.j@hotmail.fr>
2025-06-20 15:03:00 +0200

    redux part2

--- react_redux-part2/task_0/dashboard/src/components/Notifications/Notifications.jsx
+++ react_redux-part2/task_0/dashboard/src/components/Notifications/Notifications.jsx
@@ -1,4 +1,4 @@
-import { memo, useEffect, useRef, useState } from "react";
+import { memo, useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { StyleSheet, css } from "aphrodite";
 import closeIcon from "../../assets/close-icon.png";
@@ -14,8 +14,7 @@ const Notifications = memo(function Notifications() {
     (state) => state.notifications.notifications
   );
 
-  const drawerRef = useRef(null);
-  const [isVisible, setIsVisible] = useState(false); // Local UI state
+  const [isVisible, setIsVisible] = useState(false); // Local state
 
   useEffect(() => {
     dispatch(fetchNotifications());
@@ -32,42 +31,42 @@ const Notifications = memo(function Notifications() {
       <div onClick={handleToggleDrawer} style={{ cursor: "pointer" }}>
         Your notifications
       </div>
-      <div
-        ref={drawerRef}
-        className={css(styles.notifications, isVisible && styles.visible)}
-        data-testid="notifications-container"
-      >
-        {notifications.length > 0 ? (
-          <>
-            <p>Here is the list of notifications</p>
-            <button onClick={handleToggleDrawer} aria-label="Close">
-              <img src={closeIcon} alt="close icon" />
-            </button>
-            <ul>
-              {notifications.map((notification) => (
-                <NotificationItem
-                  key={notification.id}
-                  id={notification.id}
-                  type={notification.type}
-                  value={notification.value}
-                  html={notification.html}
-                  markAsRead={() => handleMarkAsRead(notification.id)}
-                />
-              ))}
-            </ul>
-          </>
-        ) : (
-          <p>No new notifications for now</p>
-        )}
-      </div>
+
+      {isVisible && (
+        <div
+          className={css(styles.notifications, styles.visible)}
+          data-testid="notifications-container"
+        >
+          {notifications.length > 0 ? (
+            <>
+              <p>Here is the list of notifications</p>
+              <button onClick={handleToggleDrawer} aria-label="Close">
+                <img src={closeIcon} alt="close icon" />
+              </button>
+              <ul>
+                {notifications.map((notification) => (
+                  <NotificationItem
+                    key={notification.id}
+                    id={notification.id}
+                    type={notification.type}
+                    value={notification.value}
+                    html={notification.html}
+                    markAsRead={() => handleMarkAsRead(notification.id)}
+                  />
+                ))}
+              </ul>
+            </>
+          ) : (
+            <p>No new notifications for now</p>
+          )}
+        </div>
+      )}
     </>
   );
 });
 
 const styles = StyleSheet.create({
   notifications: {
-    opacity: 0,
-    visibility: "hidden",
     transition: "opacity 0.3s ease-in-out, visibility 0.3s ease-in-out",
     border: "1px dashed crimson",
     padding: "1rem",
